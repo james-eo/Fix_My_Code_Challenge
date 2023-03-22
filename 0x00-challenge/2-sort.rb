d3b2ba93f769a8420d627b01935128306a1f1258
james-eo <jameseokonkwo@gmail.com>
2023-03-22 15:21:15 +0100

    Sorting arguments in ruby

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -18,12 +18,11 @@ ARGV.each do |arg|
     l = result.size
     while !is_inserted && i < l do
         if result[i] > i_arg
-            i += 1
-        else
             result.insert(i, i_arg)
             is_inserted = true
             break
         end
+        i += 1
     end
     result << i_arg if !is_inserted
 end
